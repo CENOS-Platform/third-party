74dbea15d3623990c56afb491261ca3b3a0ec6db
vgeza <vg@cenos-platform.com>
2024-10-30 20:32:14 +0200

    CP-6736 Update Netgen (0a2479bf)

--- Netgen/Netgen-Release/cmake/netgen-targets-release.cmake
+++ Netgen/Netgen-Release/cmake/netgen-targets-release.cmake
@@ -15,25 +15,6 @@ set_target_properties(ngcore PROPERTIES
 list(APPEND _IMPORT_CHECK_TARGETS ngcore )
 list(APPEND _IMPORT_CHECK_FILES_FOR_ngcore "${_IMPORT_PREFIX}/lib/ngcore.lib" "${_IMPORT_PREFIX}/bin/ngcore.dll" )
 
-# Import target "netgen" for configuration "Release"
-set_property(TARGET netgen APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
-set_target_properties(netgen PROPERTIES
-  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/bin/netgen.exe"
-  )
-
-list(APPEND _IMPORT_CHECK_TARGETS netgen )
-list(APPEND _IMPORT_CHECK_FILES_FOR_netgen "${_IMPORT_PREFIX}/bin/netgen.exe" )
-
-# Import target "nggui" for configuration "Release"
-set_property(TARGET nggui APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
-set_target_properties(nggui PROPERTIES
-  IMPORTED_IMPLIB_RELEASE "${_IMPORT_PREFIX}/lib/libnggui.lib"
-  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/bin/libnggui.dll"
-  )
-
-list(APPEND _IMPORT_CHECK_TARGETS nggui )
-list(APPEND _IMPORT_CHECK_FILES_FOR_nggui "${_IMPORT_PREFIX}/lib/libnggui.lib" "${_IMPORT_PREFIX}/bin/libnggui.dll" )
-
 # Import target "ngpy" for configuration "Release"
 set_property(TARGET ngpy APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
 set_target_properties(ngpy PROPERTIES
@@ -43,26 +24,6 @@ set_target_properties(ngpy PROPERTIES
 
 list(APPEND _IMPORT_CHECK_TARGETS ngpy )
 list(APPEND _IMPORT_CHECK_FILES_FOR_ngpy "${_IMPORT_PREFIX}/Lib/site-packages/netgen/libngpy.lib" "${_IMPORT_PREFIX}/Lib/site-packages/netgen/libngpy.pyd" )
-
-# Import target "ngguipy" for configuration "Release"
-set_property(TARGET ngguipy APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
-set_target_properties(ngguipy PROPERTIES
-  IMPORTED_IMPLIB_RELEASE "${_IMPORT_PREFIX}/Lib/site-packages/netgen/libngguipy.lib"
-  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/Lib/site-packages/netgen/libngguipy.pyd"
-  )
-
-list(APPEND _IMPORT_CHECK_TARGETS ngguipy )
-list(APPEND _IMPORT_CHECK_FILES_FOR_ngguipy "${_IMPORT_PREFIX}/Lib/site-packages/netgen/libngguipy.lib" "${_IMPORT_PREFIX}/Lib/site-packages/netgen/libngguipy.pyd" )
-
-# Import target "togl" for configuration "Release"
-set_property(TARGET togl APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
-set_target_properties(togl PROPERTIES
-  IMPORTED_IMPLIB_RELEASE "${_IMPORT_PREFIX}/lib/togl.lib"
-  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/bin/togl.dll"
-  )
-
-list(APPEND _IMPORT_CHECK_TARGETS togl )
-list(APPEND _IMPORT_CHECK_FILES_FOR_togl "${_IMPORT_PREFIX}/lib/togl.lib" "${_IMPORT_PREFIX}/bin/togl.dll" )
 
 # Import target "nglib" for configuration "Release"
 set_property(TARGET nglib APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
